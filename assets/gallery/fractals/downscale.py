cdfd8451f6997694ee3f1e53abc464607cc08207
davidsaltacc <d.lumpi@outlook.de>
2023-10-23 18:25:50 +0200

    add a fractal to gallery so it seems like i did something

--- assets/gallery/fractals/downscale.py
+++ assets/gallery/fractals/downscale.py
@@ -1,7 +1,7 @@
 import cv2
 
-for i in range(0, 49 + 1):
-    print(f"image {i}")
+for i in range(0, 50 + 1):
+    print(f"img {i}")
     img = cv2.imread(f"fractal ({i}).png")
     img = cv2.resize(img, (500, 500))
     cv2.imwrite(f"downscaled/fractal ({i}) downscaled.png", img)
