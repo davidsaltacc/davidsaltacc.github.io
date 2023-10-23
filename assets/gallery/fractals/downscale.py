7ec5b2a2ef5ebc1fbcbc03f6994d446d6981ccb5
davidsaltacc <d.lumpi@outlook.de>
2023-10-23 16:11:23 +0200

    changed the downscaled images' size from 400 to 500. why did i choose 400 itfp

--- assets/gallery/fractals/downscale.py
+++ assets/gallery/fractals/downscale.py
@@ -3,5 +3,5 @@ import cv2
 for i in range(0, 49 + 1):
     print(f"image {i}")
     img = cv2.imread(f"fractal ({i}).png")
-    img = cv2.resize(img, (400, 400))
+    img = cv2.resize(img, (500, 500))
     cv2.imwrite(f"downscaled/fractal ({i}) downscaled.png", img)
