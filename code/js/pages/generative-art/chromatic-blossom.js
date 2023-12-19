2daa74531ab4df99fdf3bbbd52115d2945787a9c
davidsaltacc <d.lumpi@outlook.de>
2023-12-19 17:15:29 +0100

    add chromatic blossom

---
+++ code/js/pages/generative-art/chromatic-blossom.js
@@ -0,0 +1,259 @@
+
+async function init() {
+
+if (!navigator.gpu) {
+    alert("WebGPU not supported on your browser. Go back to possibly supported WebGL version?");
+    return;
+}
+
+const canvasMain = document.getElementById("canvasMain");
+const contextMain = canvasMain.getContext("webgpu");
+
+var adapter = await navigator.gpu.requestAdapter();
+var device = await adapter.requestDevice();
+
+device.lost.then(() => {
+    alert("Lost contact to your GPU. Please reload this page, and if neccessary, restart your browser.");
+});
+
+const format = navigator.gpu.getPreferredCanvasFormat();
+const uniformBufferSize = Math.ceil((
+    + 2 * Float32Array.BYTES_PER_ELEMENT // center: vec2<f32>
+    + 2 * Float32Array.BYTES_PER_ELEMENT // canvasDimensions: vec2<f32>
+    + Float32Array.BYTES_PER_ELEMENT // zoom: f32
+    + 4 * Float32Array.BYTES_PER_ELEMENT // a, b, c, d : f32
+    + Uint32Array.BYTES_PER_ELEMENT // maxIterations: u32
+    + Uint32Array.BYTES_PER_ELEMENT // radius: u32
+) / 8) * 8;
+
+const AA = 4;
+
+const uniformBuffer = device.createBuffer({
+    size: uniformBufferSize,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+
+var code = null;
+await fetch("../../code/wgl/pages/generative-art/chromatic-blossom.wgsl") 
+    .then(response => response.text())
+    .then(data => {
+        code = data;
+    }
+);
+
+const shaderModule = device.createShaderModule({ code: code });
+
+const pipeline = await device.createRenderPipelineAsync({
+    layout: device.createPipelineLayout({ bindGroupLayouts: [
+        device.createBindGroupLayout({
+            entries: [
+                {
+                    binding: 0,
+                    visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+                    buffer: {},
+                }
+            ]
+        })
+    ]}),
+    vertex: {
+        module: shaderModule,
+        entryPoint: "vertex",
+    },
+    fragment: {
+        module: shaderModule,
+        entryPoint: "fragment",
+        targets: [{ format }],
+    },
+    primitive: {
+        topology: "triangle-strip",
+    },
+    multisample: {
+        count: AA
+    }
+});
+
+const bindGroup = device.createBindGroup({
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [
+        {
+            binding: 0,
+            resource: {
+                buffer: uniformBuffer
+            }
+        }
+    ]
+});
+
+contextMain.configure({
+    device,
+    format,
+    alphaMode: "opaque",
+});
+
+var centerMain = [0, 0];
+var zoomMain = 1 / 2.5;
+
+var maxIterations = 20;
+var radius = 100;
+
+var a = 0.5;
+var b = 0.5;
+var c = 8;
+var d = 4;
+
+const texture = device.createTexture({
+    size: [canvasMain.clientWidth, canvasMain.clientHeight],
+    sampleCount: AA,
+    format: format,
+    usage: GPUTextureUsage.RENDER_ATTACHMENT
+});
+const view = texture.createView();
+
+function draw(context, center, zoom) {
+	const arrayBuffer = new ArrayBuffer(uniformBufferSize);
+	new Float32Array(arrayBuffer, 0).set([
+		center[0],
+        center[1],
+        canvasMain.clientWidth, 
+        canvasMain.clientHeight,
+        zoom,
+        a, b, c, d
+	]);
+	new Uint32Array(arrayBuffer, 9 * Float32Array.BYTES_PER_ELEMENT).set([
+        maxIterations,
+        radius
+    ]);
+	device.queue.writeBuffer(uniformBuffer, 0, arrayBuffer);
+
+	const encoder = device.createCommandEncoder();
+	const renderPass = encoder.beginRenderPass({
+		colorAttachments: [{
+			view,
+            resolveTarget: context.getCurrentTexture().createView(),
+			loadOp: "clear",
+			clearValue: [0, 0, 0, 1],
+			storeOp: "store"
+		}]
+	});
+
+	renderPass.setPipeline(pipeline);
+	renderPass.setBindGroup(0, bindGroup);
+	renderPass.draw(4);
+	renderPass.end();
+
+	device.queue.submit([encoder.finish()]);
+};
+
+function renderMain() {
+    draw(contextMain, centerMain, zoomMain);
+}
+
+renderMain();
+
+var mouse_x_main = 0;
+var mouse_y_main = 0;
+var mouse_clicked_main = false;
+var mouse_clicked_right_main = false;
+function updateMouseCoords_main(e) {
+    mouse_x_main = (2 * (e.pageX - e.target.offsetLeft) - canvasMain.clientWidth ) / Math.min(canvasMain.clientWidth, canvasMain.clientHeight);
+    mouse_y_main = (2 * (e.pageY - e.target.offsetTop ) - canvasMain.clientHeight) / Math.min(canvasMain.clientWidth, canvasMain.clientHeight);
+    mouse_y_main *= -1;
+    mouse_x_main = mouse_x_main / zoomMain + centerMain[0];
+    mouse_y_main = mouse_y_main / zoomMain + centerMain[1];
+}
+function on_zoom_main(e) {
+    e.preventDefault();
+    var z = Math.exp(-e.deltaY / 500);
+    updateMouseCoords_main(e);
+    centerMain[0] = mouse_x_main + (centerMain[0] - mouse_x_main) / z;
+    centerMain[1] = mouse_y_main + (centerMain[1] - mouse_y_main) / z;
+    zoomMain *= z;
+    renderMain();
+}
+function mouse_down_main(e) {
+    if (e.button == 2) {
+        mouse_clicked_right_main = true;
+    }
+    updateMouseCoords_main(e);
+}
+function mouse_up_main(e) {
+    if (e.button == 0) {
+        mouse_clicked_main = false;
+        
+    }
+    if (e.button == 2) {
+        mouse_clicked_right_main = false;
+    }
+}
+function mouse_move_main(e) {
+    if (mouse_clicked_main) {
+        updateMouseCoords_main(e);
+        juliasetConstant[0] = mouse_x_main;
+        juliasetConstant[1] = mouse_y_main;
+        updateUi();
+        renderJul();
+    }
+    if (mouse_clicked_right_main) {
+        var old_x = mouse_x_main;
+        var old_y = mouse_y_main;
+        updateMouseCoords_main(e);
+        centerMain[0] += old_x - mouse_x_main;
+        centerMain[1] += old_y - mouse_y_main;
+        updateMouseCoords_main(e);
+        renderMain();
+    }
+}
+
+canvasMain.onwheel = on_zoom_main;
+canvasMain.onmousedown = mouse_down_main;
+canvasMain.onmouseup = mouse_up_main;
+canvasMain.onmousemove = mouse_move_main;
+
+canvasMain.oncontextmenu = function(e) { e.preventDefault() };
+
+function exportMain() {
+    var data = canvasMain.toDataURL("image/png");
+    var a = document.createElement("a");
+    a.href = data;
+    a.download = "thing.png";
+    a.click();
+    a.remove();
+}
+
+function setCanvasSize(size) {
+    canvasMain.width = canvasMain.height = size;
+    canvasJul.width = canvasJul.height = size;
+    canvasMain.clientWidth = canvasMain.clientHeight = size;
+    canvasJul.clientWidth = canvasJul.clientHeight = size;
+    renderMain();
+}
+
+function setIterations(i) { maxIterations = i; renderMain(); }
+function setRadius(r) { radius = r; renderMain(); }
+
+function setA(a_) { a = a_; renderMain(); }
+function setB(b_) { b = b_; renderMain(); }
+function setC(c_) { c = c_; renderMain(); }
+function setD(d_) { d = d_; renderMain(); }
+
+return [renderMain, exportMain, setCanvasSize, setIterations, setRadius, setA, setB, setC, setD];
+}
+
+var renderMain;
+var exportMain;
+var setCanvasSize; 
+var setIterations; 
+var setRadius; 
+var setA, setB, setC, setD; 
+(async () => { return await init(); })().then(([renderMain2, exportMain2, setCanvasSize2, setIterations2, setRadius2, setA2, setB2, setC2, setD2]) => {
+    renderMain = renderMain2;
+    exportMain = exportMain2;
+    setCanvasSize = setCanvasSize2;
+    setIterations = setIterations2;
+    setRadius = setRadius2;
+    setA = setA2;
+    setB = setB2;
+    setC = setC2;
+    setD = setD2;
+});
+
---
+++ code/wgl/pages/generative-art/chromatic-blossom.wgsl
@@ -0,0 +1,73 @@
+
+struct VertexOutput {
+	@builtin(position) position: vec4<f32>,
+	@location(0) fragmentPosition: vec2<f32>,
+}
+
+struct Uniforms {
+	center: vec2<f32>,
+	canvasDimensions: vec2<f32>,
+	zoom: f32,
+	a: f32,
+	b: f32,
+	c: f32,
+	d: f32,
+	maxIterations: u32,
+    radius: u32
+}
+
+@group(0) @binding(0) var<uniform> uniforms: Uniforms;
+
+const pi: f32 = 3.1415926535897932;
+
+@vertex
+fn vertex(@builtin(vertex_index) vertexIndex: u32) -> VertexOutput {
+	var output: VertexOutput;
+	var positions: array<vec2<f32>, 4> = array<vec2<f32>, 4>(
+		vec2<f32>(1.0, -1.0),
+		vec2<f32>(1.0, 1.0),
+		vec2<f32>(-1.0, -1.0),
+		vec2<f32>(-1.0, 1.0),
+	);
+	let position2d: vec2<f32> = positions[vertexIndex];
+	output.position = 0.5 * vec4<f32>(position2d, 0.0, 1.0);
+	output.fragmentPosition = position2d;
+	return output;
+}
+
+@fragment
+fn fragment(input: VertexOutput) -> @location(0) vec4<f32> { 
+
+	var a: f32 = uniforms.a;
+	var b: f32 = uniforms.b;
+	var c: f32 = uniforms.c;
+	var d: f32 = uniforms.d;
+
+	var window: vec2<f32> = uniforms.canvasDimensions / min(uniforms.canvasDimensions.x, uniforms.canvasDimensions.y);
+	var rc: vec2<f32> = input.fragmentPosition * window / uniforms.zoom + uniforms.center;
+    var cx: f32 = rc.x;
+    var cy: f32 = -rc.y;
+    var cz: f32 = a;
+    var cr: f32 = b;
+    for (var i: u32 = 0; i < uniforms.maxIterations; i++) {
+        var fi: f32 = f32(i);
+        var r2: f32 = cx * cx + cy * cy + cz * cz;
+        if (r2 > f32(uniforms.radius)) {
+            break;
+        } 
+        var th: f32 = atan2(sqrt(cx * cx + cy * cy), cz);
+        var ph: f32 = atan2(cy, cx);
+        var r: f32 = cr * pow(r2, 1 / c);
+        cx = r * sin(th * c + fi / d) * cos(ph * c + fi / d) + cx;
+        cy = r * sin(th * c + fi / d) * sin(ph * c + fi / d) + cy;
+        cz = r * cos(th * c + fi / d) + cz;
+    }
+	
+    return vec4<f32>(
+        cx,
+		cy,
+		cz,
+		1.0,
+	);
+}
+
