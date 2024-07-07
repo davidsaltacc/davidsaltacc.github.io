3d68145e60e5f793cca3d68eae0c7a14b9e19585
david <d.lumpi@outlook.de>
2024-07-07 14:10:02 +0200

    takes up way too much space

--- assets/gallery/fractals/downscale.py
+++
@@ -1,7 +0,0 @@
-import cv2
-
-for i in range(0, 56 + 1):
-    print(f"img {i}")
-    img = cv2.imread(f"fractal ({i}).png")
-    img = cv2.resize(img, (500, 500))
-    cv2.imwrite(f"downscaled/fractal ({i}) downscaled.png", img)
